1ca507dea469366fc84015586a082eb32e525d6b
Saurabh Hardel <saurabhhardel5286@gmail.com>
2025-04-10 15:42:33 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,20 +1,25 @@
-import React from 'react' //rfce
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Certificates from './components/Certificates'
-import Contact from './components/Contact'
+// filepath: c:\Users\Arun\Downloads\portfolio2\portfolio~\portfolio2\src\App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Certificates from './components/Certificates';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div>
-      
+    <Router>
       <Navbar />
-      <Home />
-      <Certificates />
-      <Contact />
-    
-    </div>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/certificates" element={<Certificates />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
